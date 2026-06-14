05c08d66853fce51d3ca07be206e4e3ba266a45a
Nyangta <ckddus0926@gmail.com>
2026-06-14 17:26:01 +0900

    feat: MooDI 로고 이미지 교체 (홀로그램 버블 로고 적용)

--- frontend/src/components/MoodiLogo.jsx
+++ frontend/src/components/MoodiLogo.jsx
@@ -1,14 +1,15 @@
+import logoImg from '../assets/moodi-logo.jpg';
 import '../styles/logo.css';
 
 export default function MoodiLogo() {
   return (
     <div className="moodi-logo">
-      <div className="moodi-logo__circle">
-        <span className="moodi-logo__note moodi-logo__note--1">♪</span>
-        <span className="moodi-logo__note moodi-logo__note--2">♫</span>
-        <span className="moodi-logo__note moodi-logo__note--3">♩</span>
-        <span className="moodi-logo__text">MooDI</span>
-      </div>
+      <img
+        src={logoImg}
+        alt="MooDI 로고"
+        className="moodi-logo__img"
+        draggable={false}
+      />
       <p className="moodi-logo__subtitle">감정으로 찾는 나만의 음악</p>
     </div>
   );
